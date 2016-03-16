8df2f74f9fc04e4f3da8f32a16df6a685925745a
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-03-16 17:00:24 +0900

    create PostModel PostModel generated
    typo
    
    PostModel migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316074221) do
+ActiveRecord::Schema.define(version: 20160316075808) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "title",       limit: 65535
+    t.text     "body",        limit: 65535
+    t.datetime "deadline_at",                           null: false
+    t.integer  "user_id",     limit: 4
+    t.integer  "status",      limit: 1,     default: 0, null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,4 +43,5 @@ ActiveRecord::Schema.define(version: 20160316074221) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "posts", "users"
 end
