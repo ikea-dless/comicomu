277f544ca782d78e42cd0d52174df352588db696
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-04-01 16:55:21 +0900

    add commit function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @favorites = @post.favorites
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resource :favorite, module: 'fpost', only: [:show, :update, :destroy]
+  end
   devise_for :users, controllers: {
     omniauth_callbacks: 'omniauth_callbacks' }
   get 'tops/index'
