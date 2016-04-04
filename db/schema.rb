e22cb7c58560f1f671156e972e434b3e9a723b0e
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-04-04 18:15:52 +0900

    Comment model migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401063705) do
+ActiveRecord::Schema.define(version: 20160404091204) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
