45aa37efb5373a3322b07926b71530a6cba9687c
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-04-10 12:40:29 +0900

    add post_id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404091204) do
+ActiveRecord::Schema.define(version: 20160410033854) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "username",   limit: 255
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "post_id",    limit: 4
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -64,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160404091204) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "users"
   add_foreign_key "posts", "users"
