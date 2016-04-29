4875a5cba3abdc1c4189a185a745ffe185f7d03b
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-04-30 00:23:06 +0900

    refactaring

--- Capfile
+++ Capfile
@@ -4,24 +4,12 @@ require 'capistrano/setup'
 # Includes default deployment tasks
 require 'capistrano/deploy'
 
-# Includes tasks from other gems included in your Gemfile
-#
-# For documentation on these, see for example:
-#
-#   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
-#   https://github.com/capistrano/bundler
-#   https://github.com/capistrano/rails
-#
-# require 'capistrano/rvm'
 require 'capistrano/rbenv'
 
 # デプロイ先のサーバで、ユーザディレクトリでrbenvをインストールしている場合
-set :rbenv_type, :user
+set :rbenv_type, :user # :system or :user
 set :rbenv_ruby, '2.3.0'
 
-# require 'capistrano/chruby'
 require 'capistrano/bundler'
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,13 @@
-# config valid only for Capistrano 3.1
-lock '3.4.1'
-
 set :application, 'comicomu'
 set :repo_url, 'git@github.com:kazukousen/comicomu.git'
 set :branch, 'master'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/rails/comicomu'
-
-# Default value for :scm is :git
 set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
 set :log_level, :debug
-
-# Default value for :pty is false
 set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
 set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets)
 
-# Default value for default_env is {}
 set :default_env, path: '/home/guts/.rbenv/shims:/home/guts/.rbenv/bin:$PATH'
 
 # Default value for keep_releases is 5
