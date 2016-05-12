54429c8c42623fb7196c7398c2cdd5781d401b0b
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-05-12 23:23:39 +0900

    length test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe User, type: :model do
   describe 'ActiveModel' do
     it { is_expected.to validate_presence_of :username }
     it { is_expected.to validate_uniqueness_of :username }
+
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_uniqueness_of :email }
+
     it { is_expected.to validate_presence_of :password }
+    it { is_expected.to ensure_length_of(:password).is_at_least(6).is_at_most(32) }
   end
 
   describe 'ActiveRecord' do
